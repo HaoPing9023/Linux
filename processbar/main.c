a65b07b219db0ecbc1741955b6dcdd57fd458d97
Hao_Ping <2429302207@qq.com>
2025-12-19 00:37:41 +0800

    进度条ver2

--- processbar/main.c
+++ processbar/main.c
@@ -1,6 +1,24 @@
 #include"process.h"
+
+double total = 1024.0;
+double speed = 1.0;
+
+void DownLoad()
+{
+    double current = 0;
+    while(current <= total)
+    {
+        FlushProcess(total, current);
+        //下载代码
+        usleep(3000);   //充当下载数据
+        current += speed;
+    }
+    printf("\ndownload %.2lfMB Done\n", current);
+}
+
 int main()
 {
-    process_v1();
+    //process_v1();
+    DownLoad();
     return 0;
 }
--- processbar/process.c
+++ processbar/process.c
@@ -1,21 +1,45 @@
 #include"process.h"
 
-#include<unistd.h>
 #include<string.h>
 
 #define NUM 101
-#define STYLE '#'
+#define STYLE '#'   //进度条图标
+
+void FlushProcess(double total, double current)
+{
+    char buffer[NUM];   //进度条
+    memset(buffer, 0, sizeof(buffer));
+    const char* lable = "|/-\\";      //加载图标
+    int len = strlen(lable);
+
+    int num = (int)(current * 100 / total);   //进度条的进度（需要填充多少STYLE）
+    for(int i = 0; i < num; ++i)
+    {
+        buffer[i] = STYLE;
+    }
+    
+    static int cnf = 0;     //每次调用FlushProcess函数时cnf不变，目的是保持加载图标正常旋转
+    cnf %= len;
+    
+    double rate = current / total;  //加载进度
+
+    printf("[%-100s][%.1lf%%][%c]\r", buffer, rate * 100, lable[cnf++]);
+    fflush(stdout);
+}
 
 void process_v1()
 {
     int cnf = 0;
-    char buffer[NUM];
+    char buffer[NUM];   //进度条
     memset(buffer, 0, sizeof(buffer));
+    
+    const char* lable = "|/-\\";      //旋转图标
+    int len = strlen(lable);
     
     while(cnf <= 100)
     {
-        printf("[%-100s][%d%%]\r", buffer, cnf);
-        fflush(stdout);
+        printf("[%-100s][%d%%][%c]\r", buffer, cnf, lable[cnf % len]);
+        fflush(stdout);     //刷新输出流（刷新缓存）
         buffer[cnf] = STYLE;
         ++cnf;
         usleep(50000);

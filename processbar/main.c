7b83ae96fff0bfb12d45953e8a1fa5d289b0195e
Hao_Ping <2429302207@qq.com>
2025-12-20 19:30:57 +0800

    进度条完善

--- processbar/main.c
+++ processbar/main.c
@@ -3,22 +3,39 @@
 double total = 1024.0;
 double speed = 1.0;
 
-void DownLoad()
+typedef void (*callback_t)(double total, double currnt);
+
+void DownLoad(callback_t cb)
 {
     double current = 0;
     while(current <= total)
     {
-        FlushProcess(total, current);
+        cb(total, current);
         //下载代码
         usleep(3000);   //充当下载数据
         current += speed;
     }
-    printf("\ndownload %.2lfMB Done\n", current);
+    printf("\ndownload %.2lfMB Done\n", total);
+
+}
+
+void UpLoad(callback_t cb)
+{
+    double current = 0;
+    while(current <= total)
+    {
+        cb(total, current);
+        //下载代码
+        usleep(3000);   //充当下载数据
+        current += speed;
+    }
+    printf("\nupload %.2lfMB Done\n", total);
 }
 
 int main()
 {
     //process_v1();
-    DownLoad();
+    DownLoad(FlushProcess);
+    UpLoad(FlushProcess);
     return 0;
 }

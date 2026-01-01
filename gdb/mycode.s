a0b4fecaa4a476c59e8a83cb1e005cff6a23cff5
Hao_Ping <2429302207@qq.com>
2026-01-01 23:15:50 +0800

    gdb

---
+++ gdb/mycode.s
@@ -0,0 +1,148 @@
+	.file	"mycode.c"
+	.globl	flag
+	.bss
+	.align 4
+	.type	flag, @object
+	.size	flag, 4
+flag:
+	.zero	4
+	.text
+	.globl	Sum
+	.type	Sum, @function
+Sum:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movl	%esi, -24(%rbp)
+	movl	$0, -4(%rbp)
+	movl	-20(%rbp), %eax
+	movl	%eax, -8(%rbp)
+	jmp	.L2
+.L3:
+	movl	-8(%rbp), %eax
+	addl	%eax, -4(%rbp)
+	addl	$1, -8(%rbp)
+.L2:
+	movl	-8(%rbp), %eax
+	cmpl	-24(%rbp), %eax
+	jle	.L3
+	movl	flag(%rip), %eax
+	imull	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	Sum, .-Sum
+	.section	.rodata
+.LC0:
+	.string	"I will begin"
+	.align 8
+.LC1:
+	.string	"running done, result is: [%d-%d]=%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$1, -4(%rbp)
+	movl	$100, -8(%rbp)
+	movl	$.LC0, %edi
+	call	puts
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	Sum
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-44)"
+	.section	.note.GNU-stack,"",@progbits

516af98a96892c55d26a99fa0f873a7823416ece
haoping <2429302207@qq.com>
2026-01-31 10:24:38 +0800

    进程间通信——匿名管道Pipe补充

--- Pipe/testPipe.cc
+++ Pipe/testPipe.cc
@@ -1,81 +1,91 @@
 #include <iostream>
+#include <unistd.h>
 #include <cstdio>
 #include <cstring>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-using namespace std;
-
-void Child_Write(int wfd)
+// 9:
+void ChildWrite(int wfd)
 {
-    char buffer[1024];
-    int cnf = 0;
+    char c = 0;
+    int cnt = 0;
     while (true)
     {
-        snprintf(buffer, sizeof(buffer), "I am child, pid: %d, cnf: %d", getpid(), cnf++);
-        write(wfd, buffer, strlen(buffer));
-        sleep(1);
+        //snprintf(buffer, sizeof(buffer), "I am child, pid: %d, cnt: %d", getpid(), cnt++);
+        write(wfd, &c, 1);
+        printf("child: %d\n", cnt++);
+        // sleep(2);
+        //break;
+        // sleep(3);
     }
 }
 
-void Father_Read(int rfd)
+void FatherRead(int rfd)
 {
-    sleep(1);
     char buffer[1024];
     while (true)
     {
+        sleep(100);
         buffer[0] = 0;
-        ssize_t n = read(rfd, buffer, sizeof(buffer) - 1);
-        if (n > 0)
+        ssize_t n = read(rfd, buffer, sizeof(buffer)-1);
+        if(n > 0)
         {
             buffer[n] = 0;
-            cout << "Child say : " << buffer << endl;
+            std::cout << "child say: " << buffer << std::endl;
+            // sleep(2);
         }
-        else if (n == 0)
+        else if(n == 0)
         {
-            cout << "n : " << n << endl;
-            cout << "child 退出，我也退出";
+            std::cout << "n : " << n << std::endl;
+            std::cout << "child 退出，我也退出";
             break;
         }
-        else break;
+        else
+        {
+            break;
+        }
+
+        break;
     }
 }
-
 
 int main()
 {
-    int fds[2] = {0};
+    // 1. 创建管道
+    int fds[2] = {0}; // fds[0]:读端   fds[1]: 写端
     int n = pipe(fds);
     if (n < 0)
     {
-        cerr << "pipe error" << endl;
+        std::cerr << "pipe error" << std::endl;
         return 1;
     }
-    cout << "fds[0] : " << fds[0] << endl;
-    cout << "fds[1] : " << fds[1] << endl;
+    std::cout << "fds[0]: " << fds[0] << std::endl;
+    std::cout << "fds[1]: " << fds[1] << std::endl;
 
+    // 2. 创建子进程
     pid_t id = fork();
     if (id == 0)
     {
         // child
-        //  f -> r, c -> w
+        // code
+        // 3. 关闭不需要的读写端，形成通信信道
+        // f -> r, c -> w
         close(fds[0]);
-        Child_Write(fds[1]);
+        ChildWrite(fds[1]);
         close(fds[1]);
-        exit(1);
+        exit(0);
     }
-
-    // father
+    // 3. 关闭不需要的读写端，形成通信信道
+    // f -> r, c -> w
     close(fds[1]);
-    Father_Read(fds[0]);
+    FatherRead(fds[0]);
     close(fds[0]);
 
     sleep(5);
 
     int status = 0;
-    int ret = waitpid(id, &status, 0);
-
+    int ret = waitpid(id, &status, 0); // 获取到子进程的退出信息吗！！！
     if(ret > 0)
     {
         printf("exit code: %d, exit signal: %d\n", (status>>8)&0xFF, status&0x7F);
